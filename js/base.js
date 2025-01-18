385750084fb6c3c62442645d20adb7921e522a4c
belon-creator <belon8911@gmail.com>
2025-01-18 18:03:09 +0200

    qwe

---
+++ js/base.js
@@ -0,0 +1,98 @@
+// module 1
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total)
+// const diff = grapes - apples;
+// console.log(diff)
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+// const companyName = 'Cyberdyne Systems';
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+
+// const name = 'Генератор защитного поля';
+// let price = 1000;
+// console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+// price = 2000;
+// console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+// const total = 100;
+// let ordered = prompt('enter value');
+// ordered = Number(ordered)
+
+// if (ordered > total) {
+//     console.log('На складе недостаточно твоаров!');
+// } else {
+//     console.log('Заказ оформлен, с вами свяжется менеджер');
+// }
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+// let msg;
+// const userInput = prompt('enter value')
+
+// if (!userInput) {
+//     console.log('Отменено пользователем!');
+// } else if (userInput === ADMIN_PASSWORD) {
+//     console.log('Добро пожаловать!');
+// } else {
+//     console.log('Доступ запрещен, неверный пароль!');
+// }
+
+// const credits = 23580;
+// const pricePerDroid = 3500;
+// let totalPrice;
+// let inputOfUser = prompt('количество дроидов которые пользователь хочет купить');
+// inputOfUser = Number(inputOfUser);
+// if (!inputOfUser) {
+//     console.log('Отменено пользователем!');
+// } else if (inputOfUser <= 0) {
+//     console.log('Возврата нету');
+// }
+
+// else {
+//     totalPrice = pricePerDroid * inputOfUser;
+//     if (totalPrice > credits) {
+//         console.log('На вашем счету недостаточно средств!');
+//     } else {
+//         console.log(`Вы купили ${inputOfUser} дроидов за ${totalPrice} кредитов. на счету осталось ${credits - totalPrice} кредитов`);
+//     }
+
+// }
+
+// let userInput = prompt('enter your country');
+// userInput = userInput.toLowerCase();
+// let price = 0
+
+// switch (userInput) {
+//     case 'китай':
+//         price = 100;
+//         break;
+//         case 'чили':
+//             price = 1300;
+//         break;
+//         case 'австралия':
+//             price = 150;
+//         break;
+//         case 'индия':
+//             price = 200;
+//         break;
+//         case 'ямайка':
+//             price = 300;
+//         break;
+
+//     default:
+//         console.log('В вашей стране доставка не доступна');
+//         break;
+// }
+
+// console.log(`'Доставка в ${userInput} будет стоить ${price} кредитов'`);
+
+// module 2
+
