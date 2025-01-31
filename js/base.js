90edccba5abb94f9ec31aa04c279e222dbdc7b69
belon-creator <belon8911@gmail.com>
2025-01-31 19:10:53 +0200

    dfvf

--- js/base.js
+++ js/base.js
@@ -553,104 +553,383 @@
 // console.log(countUsersByCity(usersInCity));
 // { Kyiv: 2, Lviv: 2, Odesa: 1 }
 
-const numbers = [10, 25, 38, 41, 55, 62];
+// const numbers = [10, 25, 38, 41, 55, 62];
 
-function filterNumbers(arr, filterFunction) {
-  const filteredNumbers = [];
+// function filterNumbers(arr, filterFunction) {
+//   const filteredNumbers = [];
 
-  for (const num of arr) {
-    if (filterFunction(num)) {
-      filteredNumbers.push(num);
-    }
-  }
-  return filteredNumbers;
-}
+//   for (const num of arr) {
+//     if (filterFunction(num)) {
+//       filteredNumbers.push(num);
+//     }
+//   }
+//   return filteredNumbers;
+// }
 
-function isEven(num) {
-  return num % 2 === 0;
-}
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
 
-function isOdd(num) {
-  return num % 2 !== 0;
-}
+// function isOdd(num) {
+//   return num % 2 !== 0;
+// }
 
-// Виклики:
-console.log(filterNumbers(numbers, isEven)); // [10, 38, 62]
-console.log(filterNumbers(numbers, isOdd)); // [25, 41, 55]
+// // Виклики:
+// console.log(filterNumbers(numbers, isEven)); // [10, 38, 62]
+// console.log(filterNumbers(numbers, isOdd)); // [25, 41, 55]
 
-// 2
-function operate(a, b, callback) {
-  return callback(a, b);
-}
-function add(x, y) {
-  return x + y;
-}
-function multiply(x, y) {
-  return x * y;
-}
+// // 2
+// function operate(a, b, callback) {
+//   return callback(a, b);
+// }
+// function add(x, y) {
+//   return x + y;
+// }
+// function multiply(x, y) {
+//   return x * y;
+// }
 
-// Виклики:
-console.log(operate(5, 3, add)); // 8
-console.log(operate(5, 3, multiply)); // 15
+// // Виклики:
+// console.log(operate(5, 3, add)); // 8
+// console.log(operate(5, 3, multiply)); // 15
 
-// 3
+// // 3
 
-function processString(str, callback) {
-  return callback(str);
-}
+// function processString(str, callback) {
+//   return callback(str);
+// }
 
-function toUpperCase(str) {
-  return str.toUpperCase();
-}
-function reverseString(str) {
-  return str.split('').reverse().join('');
-}
+// function toUpperCase(str) {
+//   return str.toUpperCase();
+// }
+// function reverseString(str) {
+//   return str.split('').reverse().join('');
+// }
 
-// Виклики:
-console.log(processString('hello', toUpperCase)); // "HELLO"
-console.log(processString('hello', reverseString)); // "olleh"
+// // Виклики:
+// console.log(processString('hello', toUpperCase)); // "HELLO"
+// console.log(processString('hello', reverseString)); // "olleh"
 
-// 4
+// // 4
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-function forEachElement(arr, callback) {
-  for (let i = 0; i < arr.length; i += 1) {
-    callback(arr[i]);
-    console.log(arr[i]);
-  }
-}
-function logItem(item) {
-  return item;
-}
+// function forEachElement(arr, callback) {
+//   for (let i = 0; i < arr.length; i += 1) {
+//     callback(arr[i]);
+//     console.log(arr[i]);
+//   }
+// }
+// function logItem(item) {
+//   return item;
+// }
 
-// Виклик:
-forEachElement(arr, logItem);
+// // Виклик:
+// forEachElement(arr, logItem);
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+
+// // 5
+
+// const users = [
+//   { name: 'Alice', age: 25 },
+//   { name: 'Bob', age: 30 },
+//   { name: 'Charlie', age: 20 },
+// ];
+
+// function sortBy(users, key, callback) {
+//   const copyUsers = [...users];
+//   return copyUsers.sort((a, b) => callback(a[key], b[key]));
+// }
+
+// const compareAscending = (a, b) => a - b;
+// const compareDescending = (a, b) => b - a;
+
+// // Виклики:
+// console.log(sortBy(users, 'age', compareAscending));
+// // [{ name: 'Charlie', age: 20 }, { name: 'Alice', age: 25 }, { name: 'Bob', age: 30 }]
+
+// console.log(sortBy(users, 'age', compareDescending));
+// // [{ name: 'Bob', age: 30 }, { name: 'Alice', age: 25 }, { name: 'Charlie', age: 20 }]
+
 // 1
-// 2
-// 3
-// 4
-// 5
 
-// 5
+// const numbers = [10, 20, 30, 40, 50];
+// numbers.forEach(number => console.log(number));
+
+// // 2
+
+// const numbs = [1, 2, 3, 4, 5];
+// numbs.forEach(numb => console.log((numb *= 2)));
+
+// // 3
+
+// const users = [
+//   { name: 'Alice', age: 25 },
+//   { name: 'Bob', age: 30 },
+//   { name: 'Charlie', age: 20 },
+// ];
+// users.forEach(user => console.log(`${user.name}`));
+
+// // 4
+
+// const numbersToCalcTotal = [5, 10, 15, 20];
+// let total = 0;
+// numbersToCalcTotal.forEach(num => (total += num));
+// console.log(total);
+
+// // 5
+
+// const usersToId = [{ name: 'Alice' }, { name: 'Bob' }, { name: 'Charlie' }];
+
+// usersToId.forEach((user, idx) => {
+//   user.id = idx + 1;
+//   console.log(user);
+// });
+
+// // 6
+
+// const words = ['hello', 'world', 'javascript'];
+
+// words.forEach(word => console.log(word.toUpperCase()));
+
+// // 7
+
+// const wrds = ['apple', 'banana', 'cherry'];
+// const wrdLength = [];
+// wrds.forEach(word => wrdLength.push(word.length));
+// console.log(wrdLength);
+
+// // 8
+
+// const products = [
+//   { name: 'Laptop', price: 1000 },
+//   { name: 'Phone', price: 500 },
+//   { name: 'Tablet', price: 700 },
+// ];
+
+// products.forEach(product =>
+//   console.log(`${product.name}: ${product.price * 1.1}`),
+// );
+
+// // 9
+
+// const nmbrs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const even = [];
+// const odd = [];
+
+// nmbrs.forEach(numb => (numb % 2 === 0 ? even.push(numb) : odd.push(numb)));
+// console.log(even);
+// console.log(odd);
+
+// // 10
+
+// const usrs = [
+//   { name: 'Alice', age: 17 },
+//   { name: 'Bob', age: 21 },
+//   { name: 'Charlie', age: 16 },
+//   { name: 'David', age: 19 },
+// ];
+// usrs.forEach(user =>
+//   user.age >= 18
+//     ? console.log(`${user.name} повнолітній`)
+//     : console.log(`${user.name} неповнолітній`),
+// );
+
+// 1
+// const numbers = [1, 2, 3, 4, 5];
+// // Очікуваний результат: [2, 4, 6, 8, 10]
+
+// const doubledNumbers = numbers.map(number => number * 2);
+// console.log(doubledNumbers);
+
+// // 2
+
+// const words = ['apple', 'banana', 'cherry'];
+// // Очікуваний результат: [5, 6, 6]
+// const wordsLengthArr = [];
+// words.map(word => wordsLengthArr.push(word.length));
+// console.log(wordsLengthArr);
+
+// // 3
+
+// const users = [{ name: 'Alice' }, { name: 'Bob' }, { name: 'Charlie' }];
+// // Очікуваний результат: ['Привіт, Alice!', 'Привіт, Bob!', 'Привіт, Charlie!']
+// const greetMsg = users.map(user => `Привіт, ${user.name}!`);
+// console.log(greetMsg);
+
+// // 4
+
+// const celsiusTemps = [0, 20, 30, 100];
+// // Очікуваний результат: [32, 68, 86, 212]
+// const farTemps = celsiusTemps.map(temp => (temp * 9) / 5 + 32);
+// console.log(farTemps);
+
+// // 5
+
+// const usrs = [
+//   { name: 'Alice', age: 17 },
+//   { name: 'Bob', age: 21 },
+//   { name: 'Charlie', age: 16 },
+//   { name: 'David', age: 19 },
+// ];
+// // Очікуваний результат:
+// // [{ name: 'Alice', age: 17, isAdult: false }, ..., { name: 'David', age: 19, isAdult: true }]
+// const updatedUsers = usrs.map(user => {
+//   if (user.age >= 18) {
+//     return { ...user, isAdult: true };
+//   }
+//   return { ...user, isAdult: false };
+// });
+// console.log(updatedUsers);
+
+// // 6
+
+// const prices = [10, 20.5, 5.99, 100];
+// // Очікуваний результат: ['$10.00', '$20.50', '$5.99', '$100.00']
+
+// const formattedPrices = prices.map(price => `$${price.toFixed(2)}`);
+// console.log(formattedPrices);
+
+// // 7
+
+// const products = [
+//   { name: 'Laptop', price: 1000 },
+//   { name: 'Phone', price: 500 },
+//   { name: 'Tablet', price: 700 },
+// ];
+// // Очікуваний результат: ['Laptop', 'Phone', 'Tablet']
+// const productNames = products.map(product => product.name);
+// console.log(productNames);
+
+// // 8
+
+// const items = [{ name: 'Item1' }, { name: 'Item2' }, { name: 'Item3' }];
+// // Очікуваний результат:
+// // [{ name: 'Item1', id: 1 }, { name: 'Item2', id: 2 }, { name: 'Item3', id: 3 }]
+// const updatedItems = items.map((item, idx) => ({
+//   ...item,
+//   id: idx + 1,
+// }));
+// console.log(updatedItems);
+
+// // 9
+
+// const productNames1 = ['Big Laptop', 'Smart Phone', 'Wireless Mouse'];
+// // Очікуваний результат: ['big-laptop', 'smart-phone', 'wireless-mouse']
+// const urlCase = productNames1.map(product =>
+//   product.split(' ').join('-').toLowerCase(),
+// );
+// console.log(urlCase);
+
+// // 10
+
+// const people = [
+//   { firstName: 'Alice', lastName: 'Brown', age: 25 },
+//   { firstName: 'Bob', lastName: 'Smith', age: 30 },
+//   { firstName: 'Charlie', lastName: 'Adams', age: 20 },
+// ];
+// // Очікуваний результат:
+// // [{ fullName: 'Alice Brown', age: 25 }, { fullName: 'Bob Smith', age: 30 }, { fullName: 'Charlie Adams', age: 20 }]
+
+// const fullNames = people.map(person => {
+//   return {
+//     fullName: `${person.firstName} ${person.lastName}`,
+//     age: person.age,
+//   };
+// });
+
+// console.log(fullNames);
+
+// 1
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Очікуваний результат: [2, 4, 6, 8, 10]
+
+const evenArr = numbers.filter(number => number % 2 === 0);
+console.log(evenArr);
+
+// 2
+
+const words = ['apple', 'banana', 'cherry', 'kiwi', 'watermelon'];
+// Очікуваний результат: ['banana', 'cherry', 'watermelon']
+const filteredWords = words.filter(word => word.length > 5);
+console.log(filteredWords);
+
+// 3
 
 const users = [
-  { name: 'Alice', age: 25 },
-  { name: 'Bob', age: 30 },
-  { name: 'Charlie', age: 20 },
+  { name: 'Alice', age: 17 },
+  { name: 'Bob', age: 21 },
+  { name: 'Charlie', age: 16 },
+  { name: 'David', age: 19 },
 ];
+// Очікуваний результат: [{ name: 'Bob', age: 21 }, { name: 'David', age: 19 }]
+const filteredUsers = users.filter(user => user.age >= 18);
+console.log(filteredUsers);
 
-function sortBy(users, key, callback) {
-  const copyUsers = [...users];
-  return copyUsers.sort((a, b) => callback(a[key], b[key]));
-}
+// 4
 
-const compareAscending = (a, b) => a - b;
-const compareDescending = (a, b) => b - a;
+const products = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Phone', price: 800 },
+  { name: 'Tablet', price: 700 },
+];
+// Очікуваний результат: [{ name: 'Phone', price: 800 }, { name: 'Tablet', price: 700 }]
+const filteredProducts = products.filter(product => product.price < 1000);
+console.log(filteredProducts);
 
-// Виклики:
-console.log(sortBy(users, 'age', compareAscending));
-// [{ name: 'Charlie', age: 20 }, { name: 'Alice', age: 25 }, { name: 'Bob', age: 30 }]
+// 5
 
-console.log(sortBy(users, 'age', compareDescending));
-// [{ name: 'Bob', age: 30 }, { name: 'Alice', age: 25 }, { name: 'Charlie', age: 20 }]
+const nmbrs = [1, 2, 3, 2, 4, 5, 1, 6, 7, 8, 5, 9];
+// Очікуваний результат: [3, 4, 6, 7, 8, 9]
+const uniqueNumbs = nmbrs.filter((numbers, idx, arr) => arr.indexOf(numbers) === idx);
+console.log(uniqueNumbs);
+
+// 6
+const activeUsers = [
+  { name: 'Alice', isActive: true },
+  { name: 'Bob', isActive: false },
+  { name: 'Charlie', isActive: true },
+  { name: 'David', isActive: false },
+];
+// Очікуваний результат: [{ name: 'Alice', isActive: true }, { name: 'Charlie', isActive: true }]
+const filteredUsrs = activeUsers.filter(user => user.isActive)
+console.log(filteredUsrs);
+
+// 7
+
+const strings = ['hello', '', 'world', ' ', 'JavaScript', ''];
+// Очікуваний результат: ['hello', 'world', 'JavaScript']
+const fixedArr = strings.filter(string => string !== '' && string !== ' ')
+console.log(fixedArr);
+
+// 8
+
+const usrs = [
+  { name: 'Alice', city: 'Kyiv' },
+  { name: 'Bob', city: 'Lviv' },
+  { name: 'Charlie', city: 'Kyiv' },
+  { name: 'David', city: 'Odesa' },
+];
+// Очікуваний результат: [{ name: 'Alice', city: 'Kyiv' }, { name: 'Charlie', city: 'Kyiv' }]
+
+const cityOfUser = usrs.filter(user=> user.city === 'Kyiv')
+console.log(cityOfUser);
+
+// 9
+
+const wrds = ['apple', 'banana', 'cherry', 'kiwi', 'grape'];
+// Очікуваний результат: ['apple', 'banana', 'cherry', 'grape']
+const wordsWithA = wrds.filter(word => word.includes('a'))
+console.log(wordsWithA);
+
+// 10
+
+const values = [0, 1, false, 2, '', 3, null, 'hello', undefined, 'world'];
+// Очікуваний результат: [1, 2, 3, 'hello', 'world']
+const truthy = values.filter(value => value)
+console.log(truthy);
