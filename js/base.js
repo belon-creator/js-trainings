c849e2d54801d995b28aa67b190f67b76ed6ec29
belon-creator <belon8911@gmail.com>
2025-02-19 21:28:20 +0200

    sf

--- js/base.js
+++ js/base.js
@@ -2284,39 +2284,145 @@ User ${имя} is ${возраст} years old and has ${кол-во фолове
 // user.changePassword('newpassword');
 // console.log(user); // Очікувано: { name: "Alice", email: "alice@example.com", password: "newpassword" }
 
-class Car {
-  constructor({ brand, model, price, year }) {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-    this.year = year;
-  }
+// class Car {
+//   constructor({ brand, model, price, year }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//     this.year = year;
+//   }
 
-  getInfo() {
-    return `This is ${this.brand} ${this.model} ${this.year} year, cost ${this.price}$`;
-  }
+//   getInfo() {
+//     return `This is ${this.brand} ${this.model} ${this.year} year, cost ${this.price}$`;
+//   }
 
-  changePrice(newPrice) {
-    this.price = newPrice;
-  }
-}
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
 
-const tesla = new Car({
-  brand: 'Tesla',
-  model: 'X',
-  price: 50000,
-  year: 2023,
-});
+// const tesla = new Car({
+//   brand: 'Tesla',
+//   model: 'X',
+//   price: 50000,
+//   year: 2023,
+// });
 
-const audi = new Car({
-  brand: 'Audi',
-  model: 'SQ8',
-  price: 100000,
-  year: 2023,
-});
+// const audi = new Car({
+//   brand: 'Audi',
+//   model: 'SQ8',
+//   price: 100000,
+//   year: 2023,
+// });
 
-console.log(tesla.getInfo());
-console.log(audi.getInfo());
+// console.log(tesla.getInfo());
+// console.log(audi.getInfo());
 
-tesla.changePrice(80000);
-console.log(tesla.getInfo());
+// tesla.changePrice(80000);
+// console.log(tesla.getInfo());
+
+import users from '../db/users.js';
+console.log(users);
+
+const getUserNames = users => {
+  const names = [];
+  users.forEach(user => names.push(user.name));
+  return names;
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+// Задание 2
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+// Задание 3
+// Получить массив имен пользователей по полу (поле gender).
+
+const getUsersWithGender = (users, gender) => {
+  return users.filter(user => user.gender === gender);
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+// Задание 4
+// Получить массив только неактивных пользователей (поле isActive).
+
+const getInactiveUsers = users => {
+  return users.filter(user => user.isActive);
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+// Задание 5
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) => {
+  return users.find(user => user.email === email);
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+// Задание 6
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+const getUsersWithAge = (users, min, max) => {
+  const filteredUsers = [];
+  users.forEach(user => {
+    if (user.age >= min && user.age <= max) {
+      filteredUsers.push(user);
+    }
+  });
+  return filteredUsers;
+};
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+// Задание 7
+// Получить общую сумму баланса (поле balance) всех пользователей.
+
+const calculateTotalBalance = users => {
+  return users.reduce((acc, user) => (acc += user.balance), 0);
+};
+
+console.log(calculateTotalBalance(users)); // 20916
+// Задание 8
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersWithFriend = (users, friendName) => {
+  return users
+    .filter(user => user.friends.includes(friendName))
+    .map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+// Задание 9
+// Массив имен (поле name) людей, отсортированных в зависимости
+//  от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users => {
+  const copyUsers = [...users];
+  return copyUsers
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(user => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+// Задание 10
+// Получить массив всех умений всех пользователей (поле skills),
+//  при этом не должно быть повторяющихся умений и они должны быть
+//  отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = users => {
+  // твой код
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
